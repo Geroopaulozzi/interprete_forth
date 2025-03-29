9e93e2c1c73ed10f91c4bf5b42816c373b73a0e1
Geronimo <gpaulozzi@fi.uba.ar>
2025-03-29 12:19:10 -0300

    push-pop

--- src/main.rs
+++ src/main.rs
@@ -6,3 +6,21 @@ struct Pila {
     pila: Vec<i16>,
     max: usize,
 }
+
+impl Pila {
+    fn new(max: usize) -> Self {
+        Pila {
+            pila: Vec::with_capacity(max),
+            max,
+        }
+    }
+
+    fn agregar_valor(&mut self, valor: i16) {
+        self.pila.push(valor)
+    }
+    fn sacar_valor(&mut self) -> i16 {
+        if self.pila.len() > 0 {
+            self.pila.pop()
+        }
+    }
+}
